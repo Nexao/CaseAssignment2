79a1e050710a10fe03ba5b6a0a53a9d823f1ec93
Nexao <emilklausen2@hotmail.com>
2016-10-07 00:06:50 +0200

    Working in the Dom

--- src/main/webapp/js/PersonDom.js
+++ src/main/webapp/js/PersonDom.js
@@ -1,4 +1,24 @@
-function showPerson(str){
-    var http;
+$(function (){
     
-}
+    $("#submit").on("click", function(){
+        var object = new Object();
+        object.name = $("PATH FOR INPUT FIELD").val();
+        
+        var jsonObject = JSON.stringify(object);
+        var ajaxMethod = $.ajax({
+            url: "URL PATH FOR HTML",
+            contentType: "application/json",
+            data: jsonObject,
+            method: "POST"
+        });
+        
+        ajaxMethod.done(function (data){
+            console.log(data);
+            "Maybe use the for loop here - See (ajaxWithJson)";
+        });
+        
+        ajaxMethod.fail(function (jqXHR, textStatus){
+            alert("Request failed: " + textStatus);
+        });
+    });
+});
