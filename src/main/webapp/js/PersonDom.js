b5b9a2a069289d9896b0577b836a4b4635d3f451
Helge H <Helge H>
2016-10-07 09:55:19 +0200

    Hopefully committing the changes to entitites and mappings and other such related functions.

--- src/main/webapp/js/PersonDom.js
+++ src/main/webapp/js/PersonDom.js
@@ -1,12 +1,24 @@
-$.ready(function(){
-    $('#infoButton').click(function(){
+$(function (){
     
-                var temp = $.ajax({
-                    url: "api/person/getpersons",
-                    type: "GET"
-                });
-                temp.done(function(data, textStatus){
-                    $("#");
-                });
-            });
+    $("#submit").on("click", function(){
+        var object = new Object();
+        object.name = $("PATH FOR INPUT FIELD").val();
+        
+        var jsonObject = JSON.stringify(object);
+        var ajaxMethod = $.ajax({
+            url: "URL PATH FOR HTML",
+            contentType: "application/json",
+            data: jsonObject,
+            method: "POST"
+        });
+        
+        ajaxMethod.done(function (data){
+            console.log(data);
+            "Maybe use the for loop here - See (ajaxWithJson)";
+        });
+        
+        ajaxMethod.fail(function (jqXHR, textStatus){
+            alert("Request failed: " + textStatus);
+        });
+    });
 });
