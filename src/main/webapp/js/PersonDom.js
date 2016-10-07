de101b967369fe751a5941cd0ac5002b98c4c2ca
Nexao <emilklausen2@hotmail.com>
2016-10-07 11:10:24 +0200

    gotten a long way with the dom

--- src/main/webapp/js/PersonDom.js
+++ src/main/webapp/js/PersonDom.js
@@ -1,20 +1,33 @@
 $(function (){
     
     $("#submit").on("click", function(){
+        console.log("I was clicked");
         var object = new Object();
-        object.name = $("PATH FOR INPUT FIELD").val();
+        object.name = $("#firstName").val();
+        object.name = $("#lastName").val();
+        object.name = $("#email").val();
+        object.name = $("#phone").val();
+        object.name = $("#street").val();
+        object.name = $("#city").val();
+        object.name = $("#zip").val();
+        object.name = $("#additionalInfo").val();
+        object.name = $("#hooby").val();
         
         var jsonObject = JSON.stringify(object);
         var ajaxMethod = $.ajax({
-            url: "URL PATH FOR HTML",
+            url: "http://localhost:8080/CA2/api/person",
             contentType: "application/json",
             data: jsonObject,
             method: "POST"
         });
         
-        ajaxMethod.done(function (data){
-            console.log(data);
-            "Maybe use the for loop here - See (ajaxWithJson)";
+        ajaxMethod.done(function (data){ 
+            var persons = data;
+            console.log(persons);
+            persons.forEach(function(person){
+                $("#person").append("<li>" + ""+ "</li>");
+            });
+            
         });
         
         ajaxMethod.fail(function (jqXHR, textStatus){
